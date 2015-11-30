30a9a72e6f5face61639fe57fb80cae1e6e2cb33
Ivan Demidov <demidov@220-volt.ru>
2015-11-30 15:33:31 +0300

    +XO

---
+++ src/index.js
@@ -0,0 +1,56 @@
+var postcss = require('postcss');
+var balanced = require('balanced-match');
+var objectAssign = require('object-assign');
+
+var VAR_FUNC_IDENTIFIER = 'var';
+var maps = {};
+
+function resolveValue(value) {
+	var start = value.indexOf(VAR_FUNC_IDENTIFIER + '(');
+	if (start === -1) {
+		return [value];
+	}
+
+	value.match(/var\(\S*\)/g).map(function (match) {
+		var matches = balanced('(', ')', match);
+		var reg = new RegExp(VAR_FUNC_IDENTIFIER + '.\(' + matches.body + '.\)', 'g');
+		var property = maps[matches.body];
+
+		value = value.replace(reg, property);
+	});
+	return value;
+}
+
+module.exports = postcss.plugin('postcss-at-rules-variables', function (options) {
+	var DEFAULT = ({
+		options: {
+			atRules: ['for', 'if', 'else', 'each']
+		},
+		_concatRules: function (atRules) {
+			this.options.atRules = this.options.atRules.concat(atRules);
+			return this.options;
+		}
+	}._concatRules(options && options.atRules || []));
+
+	options = objectAssign({}, options, DEFAULT);
+
+	return function (css) {
+		var reg = new RegExp(options.atRules.join('|'));
+
+		css.walkRules(function (rule) {
+			if (rule.selectors[0] !== ':root') {
+				return;
+			}
+
+			rule.each(function (decl) {
+				var prop = decl.prop;
+				var value = decl.value;
+				maps[prop] = value;
+			});
+		});
+
+		css.walkAtRules(reg, function (rules) {
+			rules.params = resolveValue(rules.params);
+		});
+	};
+});
--- test/test.js
+++ test/test.js
@@ -1,64 +1,62 @@
+/* global describe it */
 var postcss = require('postcss');
-var expect  = require('chai').expect;
+var expect = require('chai').expect;
 
-var plugin = require('../');
-
+var plugin = require('../src/index');
 var test = function (input, output, opts) {
-    expect(postcss([plugin(opts) ]).process(input).css).to.eql(output);
+	expect(postcss([plugin(opts)]).process(input).css).to.eql(output);
 };
 
 describe('postcss-at-rules-variables', function () {
+	it('it change first properties for @for', function () {
+		test(
+			':root{ --from: 1; } @for $i from var(--from) to 2',
+			':root{ --from: 1; } @for $i from 1 to 2'
+		);
+	});
 
-    it('it change first properties for @for', function(){
-        test(
-            ':root{ --from: 1; } @for $i from var(--from) to 2',
-            ':root{ --from: 1; } @for $i from 1 to 2'
-        );
-    });
+	it('it change second properties for @for', function () {
+		test(
+			':root{ --to: 2; } @for $i from 1 to var(--to)',
+			':root{ --to: 2; } @for $i from 1 to 2'
+		);
+	});
 
-    it('it change second properties for @for', function(){
-        test(
-            ':root{ --to: 2; } @for $i from 1 to var(--to)',
-            ':root{ --to: 2; } @for $i from 1 to 2'
-        );
-    });
+	it('it change two properties for @for', function () {
+		test(
+			':root{ --from: 1; --to: 2; } @for $i from var(--from) to var(--to)',
+			':root{ --from: 1; --to: 2; } @for $i from 1 to 2'
+		);
+	});
 
-    it('it change two properties for @for', function(){
-        test(
-            ':root{ --from: 1; --to: 2; } @for $i from var(--from) to var(--to)',
-            ':root{ --from: 1; --to: 2; } @for $i from 1 to 2'
-        );
-    });
+	it('it change three properties for @for', function () {
+		test(
+			':root{ --from: 1; --to: 2; --step: 5 } @for $i from var(--from) to var(--to) by var(--step)',
+			':root{ --from: 1; --to: 2; --step: 5 } @for $i from 1 to 2 by 5'
+		);
+	});
 
-    it('it change three properties for @for', function(){
-        test(
-            ':root{ --from: 1; --to: 2; --step: 5 } @for $i from var(--from) to var(--to) by var(--step)',
-            ':root{ --from: 1; --to: 2; --step: 5 } @for $i from 1 to 2 by 5'
-        );
-    });
+	it('it change two properties for @if', function () {
+		test(
+			':root{ --first: 1; --second: 2; } @if var(--first) < var(--second)',
+			':root{ --first: 1; --second: 2; } @if 1 < 2',
+			{atRules: ['if']}
+		);
+	});
 
-    it('it change two properties for @if', function(){
-        test(
-            ':root{ --first: 1; --second: 2; } @if var(--first) < var(--second)',
-            ':root{ --first: 1; --second: 2; } @if 1 < 2',
-            { atRules: ['if'] }
-        );
-    });
+	it('it change two properties for @if, @else if', function () {
+		test(
+			':root{ --first: 1; --second: 2; } @if var(--first) < var(--second) { color: olive; } @else if var(--first) > var(--second) { color: red; }',
+			':root{ --first: 1; --second: 2; } @if 1 < 2 { color: olive; } @else if 1 > 2 { color: red; }',
+			{atRules: ['if', 'else']}
+		);
+	});
 
-    it('it change two properties for @if, @else if', function(){
-        test(
-            ':root{ --first: 1; --second: 2; } @if var(--first) < var(--second) { color: olive; } @else if var(--first) > var(--second) { color: red; }',
-            ':root{ --first: 1; --second: 2; } @if 1 < 2 { color: olive; } @else if 1 > 2 { color: red; }',
-            { atRules: ['if', 'else'] }
-        );
-    });
-
-    it('it change multi properties for @each', function(){
-        test(
-            ':root{ --array: foo, bar, baz; } @each $val in var(--array)',
-            ':root{ --array: foo, bar, baz; } @each $val in foo, bar, baz',
-            { atRules: ['each'] }
-        );
-    });
-
+	it('it change multi properties for @each', function () {
+		test(
+			':root{ --array: foo, bar, baz; } @each $val in var(--array)',
+			':root{ --array: foo, bar, baz; } @each $val in foo, bar, baz',
+			{atRules: ['each']}
+		);
+	});
 });
