0a65e08ea2c23408c57a0cd59439f6581dcd918d
scrum <scrum@list.ru>
2015-12-02 13:13:58 +0300

    tests migrate es2015+

--- test/test.js
+++ test/test.js
@@ -1,41 +1,41 @@
 /* global describe it */
-const postcss = require('postcss');
-const expect = require('chai').expect;
-const plugin = require('../lib/index');
+import postcss from 'postcss';
+import {expect} from 'chai';
+import plugin from '../lib/index';
 const test = (input, output, opts) => {
 	expect(postcss([plugin(opts)]).process(input).css).to.eql(output);
 };
 
-describe('postcss-at-rules-variables', function () {
-	it('it change first properties for @for', function () {
+describe('postcss-at-rules-variables', () => {
+	it('it change first properties for @for', () => {
 		test(
 			':root{ --from: 1; } @for $i from var(--from) to 2',
 			':root{ --from: 1; } @for $i from 1 to 2'
 		);
 	});
 
-	it('it change second properties for @for', function () {
+	it('it change second properties for @for', () => {
 		test(
 			':root{ --to: 2; } @for $i from 1 to var(--to)',
 			':root{ --to: 2; } @for $i from 1 to 2'
 		);
 	});
 
-	it('it change two properties for @for', function () {
+	it('it change two properties for @for', () => {
 		test(
 			':root{ --from: 1; --to: 2; } @for $i from var(--from) to var(--to)',
 			':root{ --from: 1; --to: 2; } @for $i from 1 to 2'
 		);
 	});
 
-	it('it change three properties for @for', function () {
+	it('it change three properties for @for', () => {
 		test(
 			':root{ --from: 1; --to: 2; --step: 5 } @for $i from var(--from) to var(--to) by var(--step)',
 			':root{ --from: 1; --to: 2; --step: 5 } @for $i from 1 to 2 by 5'
 		);
 	});
 
-	it('it change two properties for @if', function () {
+	it('it change two properties for @if', () => {
 		test(
 			':root{ --first: 1; --second: 2; } @if var(--first) < var(--second)',
 			':root{ --first: 1; --second: 2; } @if 1 < 2',
@@ -43,7 +43,7 @@ describe('postcss-at-rules-variables', function () {
 		);
 	});
 
-	it('it change two properties for @if, @else if', function () {
+	it('it change two properties for @if, @else if', () => {
 		test(
 			':root{ --first: 1; --second: 2; } @if var(--first) < var(--second) { color: olive; } @else if var(--first) > var(--second) { color: red; }',
 			':root{ --first: 1; --second: 2; } @if 1 < 2 { color: olive; } @else if 1 > 2 { color: red; }',
@@ -51,7 +51,7 @@ describe('postcss-at-rules-variables', function () {
 		);
 	});
 
-	it('it change multi properties for @each', function () {
+	it('it change multi properties for @each', () => {
 		test(
 			':root{ --array: foo, bar, baz; } @each $val in var(--array)',
 			':root{ --array: foo, bar, baz; } @each $val in foo, bar, baz',
